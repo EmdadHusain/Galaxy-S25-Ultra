9b1f64c17d5a8ed5ad0fb6549f216ecce2e8aab9
Emdad Husain <emdadhusainn@gmail.com>
2025-03-17 03:45:09 +0600

    added tailwindCli

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3.5,
+    spaceBetween: 20,
+    centeredSlides: false,
+    loop: false,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+    breakpoints: {
+      // when window width is <= 320px
+      0: {
+        slidesPerView: 1,
+      },
+      // when window width is < 640px
+      350: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+      },
+      // when window width is < 768px
+      560: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      // when window width is < 1024px
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      // when window width is >= 990px
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
